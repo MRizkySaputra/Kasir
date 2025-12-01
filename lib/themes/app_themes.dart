7c4fd8d08d55712bbcbedf23a5bb5a8b455f5457
MRizkySaputra <muhammadriizkysaputraa@gmail.com>
2025-12-01 11:15:17 +0700

    first commit

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+const Color primaryGreen = Color.fromARGB(255, 40, 78, 62);
+
+class AppThemes {
+  static final light = ThemeData(
+    primaryColor: primaryGreen, 
+    scaffoldBackgroundColor: Colors.white,
+    brightness: Brightness.light,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: primaryGreen,
+      primary: primaryGreen,
+      brightness: Brightness.light,
+      surface: Colors.white,
+    ),
+    cardColor: Colors.white,
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      selectedItemColor: primaryGreen,
+      unselectedItemColor: Colors.grey,
+    ),
+  );
+
+  static final dark = ThemeData(
+    primaryColor: primaryGreen,
+    scaffoldBackgroundColor: const Color(0xFF121212),
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF121212),
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: primaryGreen,
+      primary: primaryGreen,
+      brightness: Brightness.dark,
+      surface: const Color(0xFF121212),
+    ),
+    cardColor: const Color(0xFF1E1E1E),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Color(0xFF1E1E1E),
+      selectedItemColor: primaryGreen,
+      unselectedItemColor: Colors.grey,
+    ),
+  );
+}
