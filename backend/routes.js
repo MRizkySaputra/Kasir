dc424cd430bbd68733dc78ac40f3e88e279b0b0a
unknown <agungsetiawannugroho056@gmail.com>
2025-12-13 11:08:53 +0700

    update crud transaction

--- backend/routes.js
+++ backend/routes.js
@@ -4,12 +4,14 @@ const authRoute = require("./src/modules/auth/auth.route");
 const userRoute = require("./src/modules/user/user.route");
 const NotFoundError = require("./src/errors/NotFoundError");
 const productRoute = require("./src/modules/produk/product.route");
+const transaction = require("./src/modules/transaction/transaction.route");
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
 router.use("/product", productRoute);
+router.use("/transaction", transaction);
 
 router.use((req, res) => {
-  throw new NotFoundError("data tidak di temukan boy");
+  throw new NotFoundError("route tidak di temukan");
 });
 module.exports = router;
